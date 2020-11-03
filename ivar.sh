fe2b17ce3c610eeb0edba9b2d3cbe673cff641b8
Thomas Mehoke <tmehoke@gmail.com>
2020-11-03 12:58:16 -0500

    commented out nextflow install; source nf-specific conda env; add docker image to nextflow call

--- ivar.sh
+++ ivar.sh
@@ -1,11 +1,13 @@
 WORKINGDIR=`pwd`
 DATADIR=$1
 # Install Nextflow
-echo 'Installing nextflow'
-if [ ! -r $WORKINGDIR/nextflow ]
-then
-    curl -s https://get.nextflow.io | bash    
-fi
+#echo 'Installing nextflow'
+#if [ ! -r $WORKINGDIR/nextflow ]
+#then
+#    curl -s https://get.nextflow.io | bash
+#fi
 EXTRAARGS=`echo "${@:2}"`
 echo 'Extra args: ' $EXTRAARGS
-$WORKINGDIR/nextflow run connor-lab/ncov2019-artic-nf -profile conda --illumina --prefix test --directory $DATADIR $EXTRAARGS
+source /home/idies/workspace/covid19/code/nCovIllumina/bashrc
+conda activate artic-ncov2019-illumina
+nextflow run connor-lab/ncov2019-artic-nf --with-docker ncovillumina --illumina --prefix test --directory $DATADIR $EXTRAARGS
