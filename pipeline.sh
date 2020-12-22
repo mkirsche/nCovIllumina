6ad0a3403b81f6a7391a76317591958edbea6c0b
Thomas Mehoke <tmehoke@gmail.com>
2020-12-22 10:02:49 -0500

    add getopts argument parsing

--- pipeline.sh
+++ pipeline.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ### Pipeline for processing Illumina reads ###
 
 ## Set up parameters
@@ -9,8 +11,54 @@ else
     BINDIR=$(dirname "$(readlink "$0" || echo "$(echo "$0" | sed -e 's,\\,/,g')")")
 fi
 
+#------------------------------------------------------------------------------
+
+usage()
+{
+cat << EOF
+usage: $0 [options]
+
+OPTIONS:
+   -h      show this message
+   -i      path to input folder containing FASTQs
+   -o      path to folder where output 'results' folder will be placed
+   -c      path to config file for this run
+
+EOF
+}
+
+#------------------------------------------------------------------------------
+# set default values here
+CONFIG=/opt/nCovIllumina/config/illumina.txt
+
+# parse input arguments
+while getopts "hi:o:c:" OPTION
+do
+	case $OPTION in
+		h) usage; exit 1 ;;
+		i) INPUTDIR=$OPTARG ;;
+		o) OUTPUTDIR=$OPTARG ;;
+		c) CONFIG=$OPTARG ;;
+		?) usage; exit ;;
+	esac
+done
+
+# if necessary arguments are not present, display usage info and exit
+if [[ ! -s "$BINDIR/bashrc" ]]; then
+	echo "Error: BINDIR ($BINDIR) does not contain the expected bashrc file."
+	usage
+	exit 2
+fi
+
+# if necessary arguments are not present, display usage info and exit
+if [[ -z "$OUTPUTDIR" ]]; then
+	OUTPUTDIR="$INPUTDIR"
+fi
+
+#------------------------------------------------------------------------------
+
 # Load parameters from config
-source $BINDIR/bashrc
+source "$BINDIR/bashrc"
 
 # Set up script parameters based on config setup
 REFERENCE=$GENOMEDIR/$PATHOGENREF/$PRIMERVERSION/*.reference.fasta
@@ -27,24 +75,16 @@ PANGOLIN_DATA=$GENOMEDIR/$PATHOGENREF/$PRIMERVERSION/pangoLEARN/pangoLEARN/data
 NEXTSTRAIN_CLADES=$GENOMEDIR/$PATHOGENREF/$PRIMERVERSION/clades.tsv
 SNPEFF_CONFIG=$GENOMEDIR/$PATHOGENREF/$PRIMERVERSION/snpEff.config
 
-# Get input and output directories
-if [ "$#" -eq 2 ]
-then
-    INPUTDIR=$1
-    OUTPUTDIR=$2
-    CONFIG=$3
-fi
-
-cd $OUTPUTDIR
-source $CONFIG
+cd "$OUTPUTDIR"
+source "$CONFIG"
 
 ## Load submodule
-if [ ! -r $BINDIR/VariantValidator/README.md ]
+if [ ! -r "$BINDIR/VariantValidator/README.md" ]
 then
   echo 'Downloading VariantValidator submodule'
-  cd $BINDIR
+  cd "$BINDIR"
   git submodule update --init --recursive
-  cd $OUTPUTDIR
+  cd "$OUTPUTDIR"
 fi
 
 javac $BINDIR/VariantValidator/src/*.java
@@ -58,9 +98,10 @@ $BINDIR/src/filterreads.sh $INPUTDIR $FILTEREDINPUTDIR $BINDIR $MIN_READ_LENGTH 
 #------------------------------------------------------------------------------
 
 # Run iVar pipeline
-if [ ! -d $OUTPUTDIR/results ]
+if [ ! -d "$OUTPUTDIR/results" ]
 then
   mkdir "$OUTPUTDIR/results"
+  cp "$CONFIG" "$OUTPUTDIR/results"
   echo 'Getting ivar config'
   javac $BINDIR/src/ParseIvarConfig.java
   extraargs=`java -cp $BINDIR/src ParseIvarConfig $CONFIG`
