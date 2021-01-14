505ce031f7a420153d3e7db509677a1f806c1b05
Shirlee Wohl <24323246+shwohl@users.noreply.github.com>
2021-01-14 17:38:00 -0500

    update postfilt to deal with indels

--- src/run_postfilter.sh
+++ src/run_postfilter.sh
@@ -18,11 +18,17 @@ GLOBALDIVERSITY=$5
 KEYPOS=$6
 CASEDEFS=$7
 AMPLICONS=$8
+HOMOPOLYMERS=$9
+chromname=$(head -1 $REFERENCE | cut -c2-)
 
 # if NTC is non-empty
 if [ -n "$NTC" ]; then
 	ntc_depthfile="$DIR/samtools/$NTC.mapped.primertrimmed.sorted.del.depth"
 	ntc_bamfile="$DIR/ncovIllumina_sequenceAnalysis_trimPrimerSequences/$NTC.mapped.primertrimmed.sorted.bam"
+	if [ ! -f $ntc_depthfile ]; then
+		echo "calculating depths for NTC ($samplename)"
+		python $BINDIR/src/calc_sample_depths.py $ntc_bamfile $ntc_depthfile $chromname
+	fi
 else
 	ntc_depthfile="None"
 	ntc_bamfile="None"
@@ -35,60 +41,63 @@ if [ ! -d $outdir ]; then
 fi
 
 
-## RUN SAMTOOLS DEPTH ON ALL SAMPLES
+## RUN POSTFILTERING
 
-# loop through all samples (includes NTC if it exists)
+# loop through all samples except NTC
 for consfile in $DIR/ncovIllumina_sequenceAnalysis_makeConsensus/*.consensus.fa; do
 
-	sample=${consfile##*/}
-	samplename=${sample%%.*}
-
-	bamfile="$DIR/ncovIllumina_sequenceAnalysis_trimPrimerSequences/$samplename.mapped.primertrimmed.sorted.bam"
-	outfile="$DIR/samtools/$samplename.mapped.primertrimmed.sorted.del.depth"
-	chromname=$(head -1 $REFERENCE | cut -c2-)
-	
-	# run script
-	if [ ! -f $outfile ]; then
-		echo "calculating depths for $samplename"
-		python $BINDIR/src/calc_sample_depths.py $bamfile $outfile $chromname
-	fi
-
-done
-
-
-## RUN POSTFILTERING ON ALL SAMPLES
-
-# loop through all samples (skip NTC if it exists)
-for consfile in $DIR/ncovIllumina_sequenceAnalysis_makeConsensus/*.consensus.fa; do
-
+	# get sample name
 	sample=${consfile##*/}
 	samplename=${sample%%.*}
 
 	if [ ! "$samplename" = "$NTC" ]; then
 
-		echo "running postfiltering for $samplename"
+		# find relevant files
+		bamfile="$DIR/ncovIllumina_sequenceAnalysis_trimPrimerSequences/$samplename.mapped.primertrimmed.sorted.bam"
+		depthfile="$DIR/samtools/$samplename.mapped.primertrimmed.sorted.del.depth"
+		chromname=$(head -1 $REFERENCE | cut -c2-)
+		
+		# RUN SAMTOOLS DEPTH (all samples including NTC)
+		if [ ! -f $depthfile ]; then
+			echo "calculating depths for $samplename"
+			python $BINDIR/src/calc_sample_depths.py $bamfile $depthfile $chromname
+		fi
+ 
+		# ALIGN SAMPLE TO REFERENCE GENOME
+		if [ ! -f $outdir/$samplename.align.ref.fasta ]; then
 
-		vcffile=$DIR/merging/$samplename.all_caller_freqs.vcf
-		mpileup="$DIR/samtools/$samplename.mpileup"
-		depth="$DIR/samtools/$samplename.mapped.primertrimmed.sorted.del.depth"
-		consensus="$DIR/ncovIllumina_sequenceAnalysis_makeConsensus/$samplename.primertrimmed.consensus.fa"
+			echo "aligning $samplename to reference genome"
+			cat $REFERENCE $consfile > "$outdir/$samplename.ref.fasta" # reference must be first
+			mafft --preservecase "$outdir/$samplename.ref.fasta" > "$outdir/$samplename.align.ref.fasta"
 
-		# run script
-		python $BINDIR/src/postfilter.py \
-		--vcffile $vcffile \
-		--mpileup $mpileup \
-		--depthfile $depth \
-		--consensus $consensus \
-		--ntc-bamfile $ntc_bamfile \
-		--ntc-depthfile $ntc_depthfile \
-		--global-vars $GLOBALDIVERSITY \
-		--key-vars $KEYPOS \
-		--case-defs $CASEDEFS \
-		--ref-genome $REFERENCE \
-		--amplicons $AMPLICONS \
-		--outdir $outdir \
-		--prefix $samplename
+		fi
 
+		# RUN POSTFILTERING CODE
+		if [ ! -f $outdir/$samplename.variant_data.txt ]; then
+
+			echo "running postfiltering for $samplename"
+
+			vcffile=$DIR/merging/$samplename.all_caller_freqs.vcf
+			consensus="$DIR/ncovIllumina_sequenceAnalysis_makeConsensus/$samplename.primertrimmed.consensus.fa"
+			alignment="$DIR/postfilt/$samplename.align.ref.fasta"
+
+			# run script
+			python $BINDIR/src/pf_working.py \
+			--vcffile $vcffile \
+			--depthfile $depthfile \
+			--aln-to-ref $alignment \
+			--ntc-bamfile $ntc_bamfile \
+			--ntc-depthfile $ntc_depthfile \
+			--global-vars $GLOBALDIVERSITY \
+			--key-vars $KEYPOS \
+			--homopolymers $HOMOPOLYMERS \
+			--case-defs $CASEDEFS \
+			--amplicons $AMPLICONS \
+			--outdir $outdir \
+			--samplename $samplename
+		fi
 	fi
+
+	echo "all parts complete for $samplename"
 
 done
